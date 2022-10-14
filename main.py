b8f0e9a2752ccd7554c57c09cbb8adcef831239d
eric <cyneck@qq.com>
2022-10-14 17:01:01 +0800

    fastapi debian dockerfile镜像

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ async def say_hello(name: str):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app=app, host="127.0.0.1", port=8000, reload=True, debug=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, debug=True)
